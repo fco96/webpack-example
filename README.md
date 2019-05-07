9d2723e48e9aa920a0e167f1306c2ee809dfffcf
Francisco <fco.olivaress@gmail.com>
2019-05-07 12:09:59 -0400

    Refactor scripts & plugins

--- README.md
+++ README.md
@@ -10,7 +10,3 @@ Para levantar el servidor introduzca los siguientes comandos
 ```
     npm run start
 ```
-
-## Observaciones
-
-En la configuración del webpack se establece que en `http://localhost:8080/` se va hostear el js generado. En el **index.html** se dice que se va a cargar el js desde `src="http://localhost:8080/app.js"`
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,52 @@
 const path = require('path')
-const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  mode: 'development',
   entry: {
     app: path.resolve(__dirname, 'src', 'index.js'),
   },
   devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'build'),
-    publicPath: 'http://localhost:8080/',
+    filename: '[name].js'
   },
   devServer: {
-    contentBase: path.join(__dirname, 'src'),
+    contentBase: path.join(__dirname, 'build'),
     port: 8080,
-    headers: { 'Access-Control-Allow-Origin': 'http://localhost:8080' },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+    }
   },
-  watch: true,
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+        ],
+      },
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ]
 }
